25ed4b7497e6ac23525cda809eb687a802c9d2af
AndrToscano <andro_ozp@hotmail.com>
2024-04-27 13:40:12 -0500

    Login - MaterialDesign

--- app/src/main/java/com/toscano/proyecto1/MainActivity.kt
+++ app/src/main/java/com/toscano/proyecto1/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.toscano.proyecto1
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +22,23 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        var edtxtUser = findViewById<EditText>(R.id.edtxtUser)
+        var edtxtPassword = findViewById<EditText>(R.id.edtxtPass)
+        var btnLogin = findViewById<Button>(R.id.btn)
+
+        btnLogin.setOnClickListener{
+            if (edtxtUser.text.toString() == "admin" && edtxtPassword.text.toString() == "admin"){
+                Snackbar.make(edtxtUser, "Bienvenido", Snackbar.LENGTH_SHORT).show()
+                //Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
+            }
+
+
+            else{
+                Snackbar.make(edtxtUser, "Hay un error en el usuario", Snackbar.LENGTH_SHORT).show()
+                //Toast.makeText(this, "Hay un error en el usuario", Toast.LENGTH_SHORT).show()
+        }
         }
     }
 }
